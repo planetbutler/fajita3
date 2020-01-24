302fa2e27955b69b31377b3f87aad435dd82c7e1
Paul <planetbutler@gmail.com>
2020-01-24 10:22:29 -0500

    trying to make maker selector work

--- src/views/maker.vue
+++ src/views/maker.vue
@@ -5,36 +5,40 @@
         <div class="ingredients">
             <div>
                 <div class="title">Meats</div>
-                <select  v-model="content" v-if content.category === "meat">
-                    <option v-for="content in contents" :key="content">{{ content.amount }} of {{ content.name }}</option>
+                <select  v-model="meat" >
+                    <option v-for="(content, index) in meatoptions" :key="index">{{ content.amount }} of {{ content.name }}</option>
                 </select>
                 <div class="title">Veggies</div>
-                <select v-model="content" >
-                    <option v-for="content in contents" :key="content">{{ content.amount }} of {{ content.name }}</option>
+                <select v-model="veggie" >
+                    <option v-for="(content, index) in veggieoptions" :key="index">{{ content.amount }} of {{ content.name }}</option>
                 </select>
                 <div class="title">Toppings</div>
-                <select v-model="content" >
-                    <option v-for="content in contents" :key="content">{{ content.amount }} of {{ content.name }}</option>
+                <select v-model="topping" >
+                    <option v-for="(content, index) in toppingoptions" :key="index">{{ content.amount }} of {{ content.name }}</option>
                 </select>
             </div> 
         </div>
         <h2>This fajita currently has:</h2>
         <div class="fajita">
             <div>A warm flour tortilla</div>   
-            <div v-show="content.length>0">{{ content }}</div>
+            <div v-show="this.meatoptions.length>0">{{ content }}</div>
+            <div v-show="this.veggieoptions.length>0">{{ content }}</div>
+            <div v-show="this.toppingoptions.length>0">{{ content }}</div>
             <button @click="addComplita">Wrap this fajita up!</button>
         </div>
-        <div v-if="complitas.length>0" class="complita.id">
+        <!--<div v-if="complitas.length>0" class="complita.id">
             <h2>Complitas list</h2>
             <div v-for="complita in complitas" :key="complita.id">
                 {{ complita.id }} - Fajita with {{ complita.content }}
             </div>
-        </div>        
+        </div>--> 
     </div>
 </template>
 
 <script>
     import {mapState} from 'vuex'
+    /*import fajitaLatorVue from '../components/fajita-lator.vue'
+-fajitaLatorVue*/
 
 export default {
     name: 'FajitaMaker',
@@ -48,10 +52,17 @@ export default {
     },
     data() {
         return {
-            content:''
+            meat:{name:null, amount:null, condition:null, category:null},
+            veggie:{name:null, amount:null, condition:null, category:null},
+            topping:{name:null, amount:null, condition:null, category:null},
+            meatoptions:[],
+            veggieoptions:[],
+            toppingoptions:[],
         }
     },
-    created() {
+    created() {this.meatoptions=this.contents.filter(item => item.category==='meat');
+                this.veggieoptions=this.contents.filter(item => item.category==='veggie');
+                this.toppingoptions=this.contents.filter(item => item.category==='topping');
     },
     mounted () {
     },
